16376e5129f68e25328e35441b33a85b01f421cc
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-07 14:40:42 +0900

    chore: add YasminArroyoGetActivities#build_request

--- yasmin-arroyo-get-activities/handler.rb
+++ yasmin-arroyo-get-activities/handler.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
+require 'net/http'
 require 'uri'
 
 module YasminArroyoGetActivities
   def handle(event:, context:)
     parent_project_id = event["pathParameters"]["project_id"]
     api_token = event["queryStringParameters"]["api_token"]
+  end
+
+  def build_request(parent_project_id:, api_token:)
+    uri = activities_url_by_parent_project_id(parent_project_id)
+    request = Net::HTTP::Get.new(uri)
+    request['Authorization'] = bearer_token(api_token)
+    request
   end
 
   private
