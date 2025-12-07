b674961f857f2c80e10e73cebfc99d72b18a7b91
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-07 14:28:56 +0900

    chore: add spec yasmin-arroyo-get-activities/handler_spec.rb

---
+++ spec/yasmin-arroyo-get-activities/handler_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative '../../yasmin-arroyo-get-activities/handler'
+
+RSpec.describe YasminArroyoGetActivities do
+  include YasminArroyoGetActivities
+end
