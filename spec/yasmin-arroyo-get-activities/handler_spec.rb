ab7e168e425bcfc609218d9fa389b74a6d607d83
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-07 14:34:19 +0900

    chore: add YasminArroyoGetActivities#activities_url_by_parent_project_id

--- spec/yasmin-arroyo-get-activities/handler_spec.rb
+++ spec/yasmin-arroyo-get-activities/handler_spec.rb
@@ -4,4 +4,15 @@ require_relative '../../yasmin-arroyo-get-activities/handler'
 
 RSpec.describe YasminArroyoGetActivities do
   include YasminArroyoGetActivities
+
+  describe '#activities_url_by_parent_project_id' do
+    it 'returns a valid URI with the parent_project_id parameter' do
+      parent_project_id = 'ffff'
+
+      uri = activities_url_by_parent_project_id(parent_project_id)
+
+      expect(uri).to be_a(URI::HTTPS)
+      expect(uri.to_s).to eq('https://api.todoist.com/api/v1/activities?parent_project_id=ffff')
+    end
+  end
 end
--- yasmin-arroyo-get-activities/handler.rb
+++ yasmin-arroyo-get-activities/handler.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+require 'uri'
+
 module YasminArroyoGetActivities
   def handle
     # TODO: Implement handler logic
+  end
+
+  private
+
+  def activities_url_by_parent_project_id(parent_project_id)
+    uri = URI('https://api.todoist.com/api/v1/activities')
+    uri.query = URI.encode_www_form({ parent_project_id: })
+    uri
   end
 end
