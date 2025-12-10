e90fecec680bc2bca34aedc9fb04f5e6e2840ea4
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-10 15:11:13 +0900

    chore: add YasminArroyoGetCompletedTasksByCompletionDate (#25)
    * chore: add YasminArroyoGetCompletedTasksByCompletionDate
    
    * fix

---
+++ spec/yasmin-arroyo-get-completed-tasks-by-completion-date/handler_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../yasmin-arroyo-get-completed-tasks-by-completion-date/handler'
+
+RSpec.describe YasminArroyoGetCompletedTasksByCompletionDate do
+  include YasminArroyoGetCompletedTasksByCompletionDate
+
+end
---
+++ yasmin-arroyo-get-completed-tasks-by-completion-date/handler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module YasminArroyoGetCompletedTasksByCompletionDate
+  module_function
+
+  def handle(event:, context:)
+  end
+end
