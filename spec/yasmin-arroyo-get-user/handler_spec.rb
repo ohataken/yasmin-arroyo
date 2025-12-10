c52dbe051460af6347f07573b3989c373e50209e
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-10 15:17:10 +0900

    chore: add YasminArroyoGetUser

---
+++ spec/yasmin-arroyo-get-user/handler_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../yasmin-arroyo-get-user/handler'
+
+RSpec.describe YasminArroyoGetUser do
+  include YasminArroyoGetUser
+
+end
---
+++ yasmin-arroyo-get-user/handler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module YasminArroyoGetUser
+  module_function
+
+  def handle(event:, context:)
+  end
+end
