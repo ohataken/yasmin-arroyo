867e6ff2afff795f40d546d028baae853d3dddd2
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-10 15:10:00 +0900

    fix

--- spec/yasmin-arroyo-get-completed-tasks-by-completion-date/handler_spec.rb
+++ spec/yasmin-arroyo-get-completed-tasks-by-completion-date/handler_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../yasmin-arroyo-get-project-activities/handler'
+require_relative '../../yasmin-arroyo-get-completed-tasks-by-completion-date/handler'
 
 RSpec.describe YasminArroyoGetCompletedTasksByCompletionDate do
   include YasminArroyoGetCompletedTasksByCompletionDate
