5d4505336a1a2c39bf2918374f7785bfa0ea6572
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-11 12:32:47 +0900

    chore: add YasminArroyoGetProjectCollaborators (#38)

---
+++ spec/yasmin-arroyo-get-project-collaborators/handler_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../yasmin-arroyo-get-project-collaborators/handler'
+
+RSpec.describe YasminArroyoGetProjectCollaborators do
+  include YasminArroyoGetProjectCollaborators
+
+end
