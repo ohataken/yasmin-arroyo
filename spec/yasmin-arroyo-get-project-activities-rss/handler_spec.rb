1a41dfa420f494f72d6789add00d1a99cce2fd43
ohataken <11092425+ohataken@users.noreply.github.com>
2025-12-11 13:07:09 +0900

    chore: add YasminArroyoGetProjectActivitiesRss

---
+++ spec/yasmin-arroyo-get-project-activities-rss/handler_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../yasmin-arroyo-get-project-activities-rss/handler'
+
+RSpec.describe YasminArroyoGetProjectActivitiesRss do
+  include YasminArroyoGetProjectActivitiesRss
+
+end
---
+++ yasmin-arroyo-get-project-activities-rss/handler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module YasminArroyoGetProjectActivitiesRss
+  module_function
+
+  def handle(event:, context:)
+  end
+end
